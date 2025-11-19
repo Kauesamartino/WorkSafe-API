cc5bd2253d53a31aa9580ad75229ca6369bb4021
Kauesamartino <kaue_samartino@hotmail.com>
2025-11-19 03:33:54 -0300

    Initialize repo and project

---
+++ src/test/java/com/worksafe/api/WorkSafeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worksafe.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkSafeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
