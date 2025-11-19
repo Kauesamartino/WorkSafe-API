148547a94d91971c06890593ff0a58bce8977c48
Kauesamartino <kaue_samartino@hotmail.com>
2025-11-19 15:03:11 -0300

    feat: add UsuarioMapper for converting UsuarioRequest to Usuario model and responses

---
+++ src/main/java/com/worksafe/api/interfaces/mapper/UsuarioMapper.java
@@ -0,0 +1,78 @@
+package com.worksafe.api.interfaces.mapper;
+
+import com.worksafe.api.domain.entity.Credenciais;
+import com.worksafe.api.domain.entity.Endereco;
+import com.worksafe.api.domain.entity.Usuario;
+import com.worksafe.api.interfaces.dto.input.UsuarioRequest;
+import com.worksafe.api.interfaces.dto.output.EnderecoResponse;
+import com.worksafe.api.interfaces.dto.output.UsuarioDetailsResponse;
+import com.worksafe.api.interfaces.dto.output.UsuarioResponse;
+
+public final class UsuarioMapper {
+
+    public static Usuario toModel(UsuarioRequest request) {
+        Credenciais credenciais = new Credenciais(
+                request.credenciais().username(),
+                request.credenciais().password(),
+                request.nome() + " " + request.sobrenome()
+        );
+
+        Endereco endereco = new Endereco(
+                request.endereco().logradouro(),
+                request.endereco().bairro(),
+                request.endereco().cep(),
+                request.endereco().numero(),
+                request.endereco().complemento(),
+                request.endereco().cidade(),
+                request.endereco().uf()
+        );
+
+        return new Usuario(
+                request.nome(),
+                request.sobrenome(),
+                request.cpf(),
+                request.email(),
+                request.telefone(),
+                credenciais,
+                request.cargo(),
+                request.departamento(),
+                endereco
+        );
+    }
+
+
+    public static UsuarioResponse toResponse(Usuario createdUsuario) {
+        EnderecoResponse endereco = new EnderecoResponse(
+                createdUsuario.getEndereco().getLogradouro(),
+                createdUsuario.getEndereco().getBairro(),
+                createdUsuario.getEndereco().getCep(),
+                createdUsuario.getEndereco().getNumero(),
+                createdUsuario.getEndereco().getComplemento(),
+                createdUsuario.getEndereco().getCidade(),
+                createdUsuario.getEndereco().getUf()
+        );
+
+        return new UsuarioResponse(
+                createdUsuario.getNome(),
+                createdUsuario.getSobrenome(),
+                createdUsuario.getCpf(),
+                createdUsuario.getEmail(),
+                createdUsuario.getTelefone(),
+                createdUsuario.getCargo(),
+                createdUsuario.getDepartamento(),
+                createdUsuario.getCreatedAt(),
+                endereco
+        );
+    }
+
+    public static UsuarioDetailsResponse toDetailsResponse(Usuario usuario) {
+        return new UsuarioDetailsResponse(
+                usuario.getNome(),
+                usuario.getSobrenome(),
+                usuario.getCpf(),
+                usuario.getCargo(),
+                usuario.getDepartamento(),
+                usuario.getCreatedAt()
+        );
+    }
+}
