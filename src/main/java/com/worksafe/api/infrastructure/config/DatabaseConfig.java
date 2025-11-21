ad6ef06764f5216a73b3ed29151c4648df2abf28
Kauesamartino <kaue_samartino@hotmail.com>
2025-11-21 03:51:21 -0300

    feat: add DatabaseConfig and SpringDocConfig for repository and API documentation configuration

---
+++ src/main/java/com/worksafe/api/infrastructure/config/DatabaseConfig.java
@@ -0,0 +1,25 @@
+package com.worksafe.api.infrastructure.config;
+
+import com.worksafe.api.domain.logging.Logger;
+import com.worksafe.api.domain.repository.UsuarioRepository;
+import com.worksafe.api.infrastructure.logging.LoggerFactory;
+import com.worksafe.api.infrastructure.persistance.UsuarioRepositoryAdapter;
+import com.worksafe.api.infrastructure.repository.JpaCredenciaisRepository;
+import com.worksafe.api.infrastructure.repository.JpaRoleRepository;
+import com.worksafe.api.infrastructure.repository.JpaUsuarioRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    public UsuarioRepository clienteRepository(JpaUsuarioRepository jpaUsuarioRepository, JpaCredenciaisRepository jpaCredenciaisRepository, JpaRoleRepository jpaRoleRepository) {
+        final Logger logger = LoggerFactory.getLogger(UsuarioRepositoryAdapter.class);
+        final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        return new UsuarioRepositoryAdapter(jpaUsuarioRepository, jpaCredenciaisRepository, jpaRoleRepository, passwordEncoder, logger);
+    }
+
+}
