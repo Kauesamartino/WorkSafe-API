3c4b070c36a1f2ca2692eac79593df2156a2cb32
Kauesamartino <kaue_samartino@hotmail.com>
2025-11-21 15:03:29 -0300

    feat: implement RecomendacaoController and RecomendacaoControllerImpl for recommendation management

---
+++ src/main/java/com/worksafe/api/interfaces/controller/RecomendacaoController.java
@@ -0,0 +1,32 @@
+package com.worksafe.api.interfaces.controller;
+
+import com.worksafe.api.interfaces.dto.input.RecomendacaoRequest;
+import com.worksafe.api.interfaces.dto.output.RecomendacaoResponse;
+import jakarta.validation.Valid;
+
+import java.util.List;
+
+public interface RecomendacaoController {
+    /**
+     * Cria uma nova recomendação com base na requisição fornecida.
+     *
+     * @param request A requisição contendo os dados da recomendação a ser criada.
+     * @return A resposta contendo os detalhes da recomendação criada.
+     */
+    RecomendacaoResponse create(@Valid RecomendacaoRequest request);
+
+    /**
+     * Busca uma recomendação pelo seu ID.
+     *
+     * @param id O ID da recomendação a ser buscada.
+     * @return A resposta contendo os detalhes da recomendação encontrada.
+     */
+    RecomendacaoResponse buscarPorId(Long id);
+
+    /**
+     * Lista todas as recomendações disponíveis.
+     *
+     * @return Uma lista de respostas contendo os detalhes de todas as recomendações.
+     */
+    List<RecomendacaoResponse> listarTodasRecomendacoes();
+}
---
+++ src/main/java/com/worksafe/api/interfaces/controller/RecomendacaoControllerImpl.java
@@ -0,0 +1,45 @@
+package com.worksafe.api.interfaces.controller;
+
+import com.worksafe.api.application.usecase.recomendacao.BuscarRecomendacaoPorIdUseCase;
+import com.worksafe.api.application.usecase.recomendacao.CreateRecomendacaoUseCase;
+import com.worksafe.api.application.usecase.recomendacao.ListarTodasRecomendacoesUseCase;
+import com.worksafe.api.domain.entity.Recomendacao;
+import com.worksafe.api.interfaces.dto.input.RecomendacaoRequest;
+import com.worksafe.api.interfaces.dto.output.RecomendacaoResponse;
+import com.worksafe.api.interfaces.mapper.RecomendacaoMapper;
+
+import java.util.List;
+
+public final class RecomendacaoControllerImpl implements  RecomendacaoController {
+
+    private final CreateRecomendacaoUseCase createRecomendacaoUseCase;
+    private final BuscarRecomendacaoPorIdUseCase buscarRecomendacaoPorIdUseCase;
+    private final ListarTodasRecomendacoesUseCase listarTodasRecomendacoesUseCase;
+
+    public RecomendacaoControllerImpl(CreateRecomendacaoUseCase createRecomendacaoUseCase, BuscarRecomendacaoPorIdUseCase buscarRecomendacaoPorIdUseCase, ListarTodasRecomendacoesUseCase listarTodasRecomendacoesUseCase) {
+        this.createRecomendacaoUseCase = createRecomendacaoUseCase;
+        this.buscarRecomendacaoPorIdUseCase = buscarRecomendacaoPorIdUseCase;
+        this.listarTodasRecomendacoesUseCase = listarTodasRecomendacoesUseCase;
+    }
+
+    @Override
+    public RecomendacaoResponse create(RecomendacaoRequest request) {
+        Recomendacao recomendacao = RecomendacaoMapper.toModel(request);
+        Recomendacao createdRecomendacao = createRecomendacaoUseCase.execute(recomendacao);
+        return RecomendacaoMapper.toResponse(createdRecomendacao);
+    }
+
+    @Override
+    public RecomendacaoResponse buscarPorId(Long id) {
+        Recomendacao recomendacao = buscarRecomendacaoPorIdUseCase.execute(id);
+        return RecomendacaoMapper.toResponse(recomendacao);
+    }
+
+    @Override
+    public List<RecomendacaoResponse> listarTodasRecomendacoes() {
+        List<Recomendacao> recomendacoes = listarTodasRecomendacoesUseCase.execute();
+        return recomendacoes.stream()
+                .map(RecomendacaoMapper::toResponse)
+                .toList();
+    }
+}
