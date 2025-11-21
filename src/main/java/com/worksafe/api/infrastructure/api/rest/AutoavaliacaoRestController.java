f9dc6a44408cd11a7fecabd039cb3c36aeb2d21d
Kauesamartino <kaue_samartino@hotmail.com>
2025-11-21 20:43:02 -0300

    feat: add AutoavaliacaoRestController for managing auto-evaluation requests

---
+++ src/main/java/com/worksafe/api/infrastructure/api/rest/AutoavaliacaoRestController.java
@@ -0,0 +1,48 @@
+package com.worksafe.api.infrastructure.api.rest;
+
+import com.worksafe.api.interfaces.controller.AutoavaliacaoController;
+import com.worksafe.api.interfaces.dto.input.AutoavaliacaoRequest;
+import com.worksafe.api.interfaces.dto.output.AutoavaliacaoResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+/**
+ * Adaptador REST para o controller de Alerta.
+ * Esta classe contém as anotações específicas do springframework e
+ * delega as chamadas para o controller puro.
+ */
+@RestController
+@RequestMapping("/autoavaliacoes")
+public class AutoavaliacaoRestController {
+
+    private final AutoavaliacaoController autoavaliacaoController;
+
+    public AutoavaliacaoRestController(AutoavaliacaoController autoavaliacaoController) {
+        this.autoavaliacaoController = autoavaliacaoController;
+    }
+
+    @PostMapping
+    public ResponseEntity<AutoavaliacaoResponse> create(@RequestBody @Valid AutoavaliacaoRequest request, UriComponentsBuilder uriComponentsBuilder) {
+        final AutoavaliacaoResponse response = autoavaliacaoController.create(request);
+        URI uri = uriComponentsBuilder.path("/autoavaliacoes/{id}")
+                .buildAndExpand(response.id())
+                .toUri();
+        return ResponseEntity.created(uri).body(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AutoavaliacaoResponse> buscarPorId(@PathVariable Long id) {
+        final AutoavaliacaoResponse response = autoavaliacaoController.buscarPorId(id);
+        return ResponseEntity.ok(response);
+    }
+
+    public ResponseEntity<List<AutoavaliacaoResponse>> listarAvaliacoes() {
+        final List<AutoavaliacaoResponse> response = autoavaliacaoController.listarAvaliacoes();
+        return ResponseEntity.ok(response);
+    }
+}
