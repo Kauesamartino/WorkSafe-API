cba3f963ed01a70e9f35ca6bf3203ed962835b80
Kauesamartino <kaue_samartino@hotmail.com>
2025-11-21 03:58:10 -0300

    feat: add documentation comment for AuthRestController to clarify its purpose

--- src/main/java/com/worksafe/api/infrastructure/api/rest/AuthRestController.java
+++ src/main/java/com/worksafe/api/infrastructure/api/rest/AuthRestController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * REST controller for authentication-related endpoints.
+ */
 @RestController
 @RequestMapping("/api/auth")
 public class AuthRestController {
