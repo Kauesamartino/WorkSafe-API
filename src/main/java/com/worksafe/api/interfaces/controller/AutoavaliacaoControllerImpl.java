e0ff3b5c4f528f24458252c5128d64eb7adb76be
Kauesamartino <kaue_samartino@hotmail.com>
2025-11-21 20:53:59 -0300

    feat: enhance AutoavaliacaoControllerImpl to include user information in responses

--- src/main/java/com/worksafe/api/interfaces/controller/AutoavaliacaoControllerImpl.java
+++ src/main/java/com/worksafe/api/interfaces/controller/AutoavaliacaoControllerImpl.java
@@ -1,7 +1,9 @@
 package com.worksafe.api.interfaces.controller;
 
 import com.worksafe.api.application.usecase.autoavaliacao.*;
+import com.worksafe.api.application.usecase.usuario.BuscarUsuarioPorIdUseCase;
 import com.worksafe.api.domain.entity.Autoavaliacao;
+import com.worksafe.api.domain.entity.Usuario;
 import com.worksafe.api.interfaces.dto.input.AutoavaliacaoRequest;
 import com.worksafe.api.interfaces.dto.output.AutoavaliacaoResponse;
 import com.worksafe.api.interfaces.mapper.AutoavaliacaoMapper;
@@ -13,31 +15,38 @@ public final class AutoavaliacaoControllerImpl implements  AutoavaliacaoControll
     private final CreateAutoavaliacaoUseCase createAutoavaliacaoUseCase;
     private final BuscarAutoavaliacaoPorIdUseCase buscarAutoavaliacaoPorIdUseCase;
     private final ListarTodasAutoavaliacoesUseCase listarTodasAutoavaliacoesUseCase;
+    private final BuscarUsuarioPorIdUseCase buscarUsuarioPorIdUseCase;
 
-    public AutoavaliacaoControllerImpl(CreateAutoavaliacaoUseCase createAutoavaliacaoUseCase, BuscarAutoavaliacaoPorIdUseCase buscarAutoavaliacaoPorIdUseCase, ListarTodasAutoavaliacoesUseCase listarTodasAutoavaliacoesUseCase) {
+    public AutoavaliacaoControllerImpl(CreateAutoavaliacaoUseCase createAutoavaliacaoUseCase, BuscarAutoavaliacaoPorIdUseCase buscarAutoavaliacaoPorIdUseCase, ListarTodasAutoavaliacoesUseCase listarTodasAutoavaliacoesUseCase, BuscarUsuarioPorIdUseCase buscarUsuarioPorIdUseCase) {
         this.createAutoavaliacaoUseCase = createAutoavaliacaoUseCase;
         this.buscarAutoavaliacaoPorIdUseCase = buscarAutoavaliacaoPorIdUseCase;
         this.listarTodasAutoavaliacoesUseCase = listarTodasAutoavaliacoesUseCase;
+        this.buscarUsuarioPorIdUseCase = buscarUsuarioPorIdUseCase;
     }
 
     @Override
     public AutoavaliacaoResponse create(AutoavaliacaoRequest request) {
         Autoavaliacao autoavaliacao = AutoavaliacaoMapper.toModel(request);
         Autoavaliacao savedAutoavaliacao = createAutoavaliacaoUseCase.execute(autoavaliacao);
-        return AutoavaliacaoMapper.toResponse(savedAutoavaliacao);
+        Usuario usuario = buscarUsuarioPorIdUseCase.execute(savedAutoavaliacao.getUsuarioId());
+        return AutoavaliacaoMapper.toResponse(savedAutoavaliacao, usuario.getNome());
     }
 
     @Override
     public AutoavaliacaoResponse buscarPorId(Long id) {
         Autoavaliacao autoavaliacao = buscarAutoavaliacaoPorIdUseCase.execute(id);
-        return AutoavaliacaoMapper.toResponse(autoavaliacao);
+        Usuario usuario = buscarUsuarioPorIdUseCase.execute(autoavaliacao.getUsuarioId());
+        return AutoavaliacaoMapper.toResponse(autoavaliacao, usuario.getNome());
     }
 
     @Override
     public List<AutoavaliacaoResponse> listarAvaliacoes() {
         return listarTodasAutoavaliacoesUseCase.execute()
                 .stream()
-                .map(AutoavaliacaoMapper::toResponse)
+                .map(autoavaliacao -> {
+                    Usuario usuario = buscarUsuarioPorIdUseCase.execute(autoavaliacao.getUsuarioId());
+                    return AutoavaliacaoMapper.toResponse(autoavaliacao, usuario.getNome());
+                })
                 .toList();
     }
 }
