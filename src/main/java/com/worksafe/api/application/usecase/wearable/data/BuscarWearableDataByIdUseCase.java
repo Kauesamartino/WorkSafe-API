a7c08a17f080b05b51237dd4d4cbf022bed8d0c4
Kauesamartino <kaue_samartino@hotmail.com>
2025-11-21 22:19:11 -0300

    feat: update ListarWearableDataUseCaseImpl to implement ListarWearableDataUseCase and return a list of WearableData

--- src/main/java/com/worksafe/api/application/usecase/wearable/data/BuscarWearableDataByIdUseCase.java
+++ src/main/java/com/worksafe/api/application/usecase/wearable/data/BuscarWearableDataByIdUseCase.java
@@ -2,6 +2,8 @@ package com.worksafe.api.application.usecase.wearable.data;
 
 import com.worksafe.api.domain.entity.WearableData;
 
+import java.util.List;
+
 public interface BuscarWearableDataByIdUseCase {
     WearableData execute(Long id);
 }
--- src/main/java/com/worksafe/api/application/usecase/wearable/data/ListarWearableDataUseCaseImpl.java
+++ src/main/java/com/worksafe/api/application/usecase/wearable/data/ListarWearableDataUseCaseImpl.java
@@ -3,7 +3,9 @@ package com.worksafe.api.application.usecase.wearable.data;
 import com.worksafe.api.domain.entity.WearableData;
 import com.worksafe.api.domain.repository.WearableDataRepository;
 
-public final class ListarWearableDataUseCaseImpl implements BuscarWearableDataByIdUseCase {
+import java.util.List;
+
+public final class ListarWearableDataUseCaseImpl implements ListarWearableDataUseCase {
 
     private final WearableDataRepository wearableDataRepository;
 
@@ -12,7 +14,7 @@ public final class ListarWearableDataUseCaseImpl implements BuscarWearableDataBy
     }
 
     @Override
-    public WearableData execute(Long id) {
-        return wearableDataRepository.findAll(id);
+    public List<WearableData> execute() {
+        return wearableDataRepository.findAll();
     }
 }
