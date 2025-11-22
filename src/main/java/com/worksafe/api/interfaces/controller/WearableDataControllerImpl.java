3d185e491fcaf50290fbe94770fbf1948b65a478
Kauesamartino <kaue_samartino@hotmail.com>
2025-11-21 21:57:42 -0300

    feat: enhance WearableDataControllerImpl to include user names in responses

--- src/main/java/com/worksafe/api/interfaces/controller/WearableDataControllerImpl.java
+++ src/main/java/com/worksafe/api/interfaces/controller/WearableDataControllerImpl.java
@@ -2,6 +2,7 @@ package com.worksafe.api.interfaces.controller;
 
 import com.worksafe.api.application.usecase.usuario.BuscarUsuarioPorIdUseCase;
 import com.worksafe.api.application.usecase.wearable.data.*;
+import com.worksafe.api.domain.entity.Usuario;
 import com.worksafe.api.domain.entity.WearableData;
 import com.worksafe.api.interfaces.dto.input.WearableRequest;
 import com.worksafe.api.interfaces.dto.output.WearableResponse;
@@ -27,20 +28,25 @@ public final class WearableDataControllerImpl implements WearableDataController 
     public WearableResponse create(WearableRequest request) {
         WearableData wearableData = WearableMapper.toModel(request);
         WearableData createdWearableData = createWearableDataUseCase.execute(wearableData);
-        return WearableMapper.toResponse(createdWearableData);
+        Usuario usuario = buscarUsuarioPorIdUseCase.execute(request.usuarioId());
+        return WearableMapper.toResponse(createdWearableData, usuario.getNome());
     }
 
     @Override
     public WearableResponse buscarPorId(Long id) {
         WearableData wearableData = buscarWearableDataByIdUseCase.execute(id);
-        return WearableMapper.toResponse(wearableData);
+        Usuario usuario = buscarUsuarioPorIdUseCase.execute(wearableData.getUsuarioId());
+        return WearableMapper.toResponse(wearableData, usuario.getNome());
     }
 
     @Override
     public List<WearableResponse> listarDados() {
         List<WearableData> wearableDataList = listarWearableDataUseCase.execute();
         return wearableDataList.stream()
-                .map(WearableMapper::toResponse)
+                .map(x -> {
+                    Usuario usuario = buscarUsuarioPorIdUseCase.execute(x.getUsuarioId());
+                    return WearableMapper.toResponse(x, usuario.getNome());
+                        })
                 .toList();
     }
 }
